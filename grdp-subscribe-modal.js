ae6c7270a9ef220d8b823b8553cdad4108847b8d
Liam <liam@melonwebdesign.co.uk>
2018-03-02 17:31:03 +0000

    * Css & form submission changes
    * CSS Changes for close button to make it easier to click
    * Change the way the form is submitted to simulate a button click (makes it more versatile for both normal and ajax requets)

--- grdp-subscribe-modal.js
+++ grdp-subscribe-modal.js
@@ -33,9 +33,9 @@ function gdprSubscribeModal(formID, privacyPolicyURL, linkColour, buttonTextColo
 		$('body').append("<section class=\"section-gdpr-subscribe-modal\" style='display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index:9999;'>\n" +
 			"\t\t\t<div class=\"sub-overlay\" style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0, 0.75); z-index: 0;'></div>\n" +
 			"\t\t\t<div class=\"sub-content\" style='position: absolute; top: 50%; left: 0; right: 0; width: 100%; max-width: 660px; margin: 0 auto; z-index: 1; background: #fff; padding: 70px 0 60px; text-align: center; transform: translateY(-50%);'>\n" +
-			"\t\t\t\t<div class=\"btn-close\" style='position: absolute; top: 25px; right: 20px; display: block;'>\n" +
-			"\t\t\t\t\t<div class=\"mod-bar bar-1\" style='background: #B3B3B3; display: block; width: 25px; height: 1.4px; transform: translate(0px, 3px) rotate(-45deg);'></div>\n" +
-			"\t\t\t\t\t<div class=\"mod-bar bar-2\" style='background: #B3B3B3; display: block; width: 25px; height: 1.4px; transform: translate(0px, 2px) rotate(45deg);'></div>\n" +
+			"\t\t\t\t<div class=\"btn-close\" style='position: absolute; height: 25px; cursor: pointer; top: 25px; right: 20px; display: block;'>\n" +
+			"\t\t\t\t\t<div class=\"mod-bar bar-1\" style='background: #B3B3B3; display: block; width: 25px; height: 1.4px; transform: translate(0px, 14px) rotate(-45deg);'></div>\n" +
+			"\t\t\t\t\t<div class=\"mod-bar bar-2\" style='background: #B3B3B3; display: block; width: 25px; height: 1.4px; transform: translate(0px, 13px) rotate(45deg);'></div>\n" +
 			"\t\t\t\t</div>\n" +
 			"\t\t\t\t<div class=\"container\" style='width: 100%; max-width: 490px; margin: 0 auto; text-align: center;'>\n" +
 			"\t\t\t\t\t<div class=\"sub-title\"><h4 style='font-size: 18px; font-weight: 500; margin: 0 0 45px;'>Thank you for your interest in our email updates.</h4></div>\n" +
@@ -54,7 +54,7 @@ function gdprSubscribeModal(formID, privacyPolicyURL, linkColour, buttonTextColo
 	$(modalSelector + ' .btn-confirm').click(function () {
 		$(modalSelector).fadeOut();
 		GDRPaccepted = true;
-		$(formID).submit();
+		$(formID + ' [type="submit"]').trigger('click')
 	});
 
 	// Fade out on btn close
